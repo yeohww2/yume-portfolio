876df9dccaceac3c024dd37c52799e38e74f6f56
Yeoh Wei Wen <139027337+yeohww2@users.noreply.github.com>
2024-11-28 15:42:33 +0800

    Update tuyu.js

--- music/tuyu/tuyu.js
+++ music/tuyu/tuyu.js
@@ -1,5 +1,1 @@
-// Remove the event listeners for the popup
-document.querySelectorAll("iframe").forEach((iframe) => {
-    iframe.removeEventListener("play", showPopup);  // No longer needed
-    iframe.removeEventListener("pause", hidePopup); // No longer needed
-});
+
